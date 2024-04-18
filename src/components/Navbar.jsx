733940924c91fa0837236b0ae67521178cc60cd4
Fabian Leiva <fabianleivacortes@gmail.com>
2024-04-18 13:43:24 -0400

    navbar correct error on burger menu closing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -122,7 +122,6 @@ const Navbar = () => {
                     <Link
                       key={item.name}
                       to={item.href}
-                      onClick={setMobileMenuOpen(false)}
                       className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                     >
                       {item.name}
