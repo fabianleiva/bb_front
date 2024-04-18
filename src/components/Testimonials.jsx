7bbc4d2ced069e77620bec73a44635ebf8b88355
Fabian Leiva <fabianleivacortes@gmail.com>
2024-04-18 16:59:25 -0400

    change text on testimonials

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -130,7 +130,7 @@ const Testimonials = () => {
               Testimonios
             </h2>
             <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-              Hemos trabajado con miles de personas increíbles
+              Hemos trabajado con miles de personas fabulosas!
             </p>
           </div>
           <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 grid-rows-1 gap-8 text-sm leading-6 text-gray-900 sm:mt-20 sm:grid-cols-2 xl:mx-0 xl:max-w-none xl:grid-flow-col xl:grid-cols-4">
