0e8cb04cf14dbe696c5ce585ce3dec42ccd69cbb
Fabian Leiva <123709798+fabianleiva@users.noreply.github.com>
2024-04-17 23:53:39 -0400

    second commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -54,7 +54,7 @@ const Navbar = () => {
             </button>
           </div>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="#" className="flex text-sm font-semibold leading-6 text-gray-700">
+            <Link to="/" className="flex text-sm font-semibold leading-6 text-gray-700">
               Buscar
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -69,17 +69,17 @@ const Navbar = () => {
                   d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
                 />
               </svg>
-            </a>
+            </Link>
           </div>
         </nav>
         <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
           <div className="fixed inset-0 z-50" />
           <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
-              <a href="#" className="-m-1.5 p-1.5">
+              <Link to="/" className="-m-1.5 p-1.5">
                 <span className="sr-only">BulkBuddies</span>
                 <img className="h-8 w-auto" src="/public/bulkbuddies_logo.png" alt="" />
-              </a>
+              </Link>
               <button
                 type="button"
                 className="-m-2.5 rounded-md p-2.5 text-gray-700"
@@ -92,20 +92,20 @@ const Navbar = () => {
               <div className="-my-6 divide-y divide-gray-500/10">
                 <div className="space-y-2 py-6">
                   {navigation.map((item) => (
-                    <a
+                    <Link
                       key={item.name}
-                      href={item.href}
+                      to={item.href}
                       className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
                       {item.name}
-                    </a>
+                    </Link>
                   ))}
                 </div>
                 <div className="py-6">
-                  <a
-                    href="#"
+                  <Link
+                    to="/auth/login"
                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
                     Log in
-                  </a>
+                  </Link>
                 </div>
               </div>
             </div>
@@ -113,12 +113,13 @@ const Navbar = () => {
         </Dialog>
       </header>
 
+{/* NAVEGACION FLOTANTE */}
       <div className="relative isolate z-50">
-        <div className="fixed bottom-8 m-auto inset-x-0 justify-between max-w-80 bg-buddies-blue-700 px-6 py-1 rounded-full hidden lg:flex lg:gap-x-12 ">
+        <div className="fixed bottom-8 m-auto inset-x-0 justify-between max-w-80 bg-buddies-blue-700 px-6 py-1 rounded-full flex lg:gap-x-12 ">
           {navigation.map((item) => (
-            <a key={item.name} href={item.href} className="PRUEBA text-sm font-light leading-6 text-buddies-bg">
+            <Link key={item.name} to={item.href} className="text-sm font-light leading-6 text-buddies-bg">
               {item.name}
-            </a>
+            </Link>
           ))}
         </div>
       </div>
