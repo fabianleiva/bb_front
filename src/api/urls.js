9a3a10e1ee78d16d2f80dc6eb2cd26795bfaf53c
Fabian Leiva <fabianleivacortes@gmail.com>
2024-04-25 21:37:57 -0400

    add urls on deploy

--- src/api/urls.js
+++ src/api/urls.js
@@ -1,2 +1,8 @@
 export const REGISTER_URL = "/register";
+export const REGISTER_GOOGLE_URL =
+  "https://bulkbuddies.onrender.com/api/v1/auth/google/register";
 export const LOGIN_URL = "/login";
+export const LOGIN_GOOGLE_URL =
+  "https://bulkbuddies.onrender.com/api/v1/auth/google";
+export const GET_POSTS_URL =
+  "https://bulkbuddies.onrender.com/api/v1/product";
