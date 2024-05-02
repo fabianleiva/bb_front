02d1a933d6be98d348c10d2290b54e63fb0f525d
Fabian Leiva <123709798+fabianleiva@users.noreply.github.com>
2024-05-01 23:56:56 -0400

    add select categories from db

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -3,10 +3,29 @@ import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { storeBulkBuddies } from "../state/state";
 import DatePicker from "./DatePicker";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { GET_CATEGORIES_URL } from "../api/urls";
+
 import dayjs from "dayjs";
 
 export const CreatePost = () => {
+  const { categories, setCategories } = storeBulkBuddies();
+
+  //Get posts and categories data*/
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const categoriesResponse = await axios.get(GET_CATEGORIES_URL);
+        setCategories(categoriesResponse.data.categories);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+  console.log(categories);
+
   const {
     handleSubmit,
     register,
@@ -50,7 +69,9 @@ export const CreatePost = () => {
       <form onSubmit={onSubmit}>
         <div className="space-y-12 sm:space-y-16">
           <div>
-            <h2 className="text-base font-semibold leading-7 text-gray-900">Información del producto</h2>
+            <h2 className="text-base font-semibold leading-7 text-gray-900">
+              Información del producto
+            </h2>
             <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-600">
               Compártenos de que trata lo que quieres comprar.
             </p>
@@ -58,7 +79,10 @@ export const CreatePost = () => {
             <div className="mt-10 space-y-8 border-b border-gray-900/10 pb-12 sm:space-y-0 sm:divide-y sm:divide-gray-900/10 sm:border-t sm:pb-0">
               {/*POST TITLE*/}
               <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
-                <label htmlFor="post_title" className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5">
+                <label
+                  htmlFor="post_title"
+                  className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
+                >
                   Título de la publicación
                 </label>
                 <div className="mt-2 sm:col-span-2 sm:mt-0">
@@ -79,7 +103,10 @@ export const CreatePost = () => {
 
               {/*PRODUCT URL*/}
               <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
-                <label htmlFor="post_url" className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5">
+                <label
+                  htmlFor="post_url"
+                  className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
+                >
                   Url del producto
                 </label>
                 <div className="mt-2 sm:col-span-2 sm:mt-0">
@@ -95,7 +122,10 @@ export const CreatePost = () => {
 
               {/*DESCRIPTION*/}
               <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
-                <label htmlFor="description" className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5">
+                <label
+                  htmlFor="description"
+                  className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
+                >
                   Descripción
                 </label>
                 <div className="mt-2 sm:col-span-2 sm:mt-0">
@@ -107,14 +137,18 @@ export const CreatePost = () => {
                     defaultValue={""}
                   />
                   <p className="mt-3 text-sm leading-6 text-gray-600">
-                    Escribe una breve descripción del producto que quieres comprar.
+                    Escribe una breve descripción del producto que quieres
+                    comprar.
                   </p>
                 </div>
               </div>
 
               {/*REREFENCE IMG*/}
               <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
-                <label htmlFor="img_url" className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5">
+                <label
+                  htmlFor="img_url"
+                  className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
+                >
                   Imagen de referencia (url)
                 </label>
                 <div className="mt-2 sm:col-span-2 sm:mt-0">
@@ -130,17 +164,24 @@ export const CreatePost = () => {
 
               {/*CATEGORY SELECT*/}
               <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
-                <label htmlFor="category" className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5">
+                <label
+                  htmlFor="category"
+                  className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
+                >
                   Categoria
                 </label>
                 <div className="mt-2">
                   <select
                     id="category"
                     name="category"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-buddies-blue-700 sm:max-w-xs sm:text-sm sm:leading-6">
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-buddies-blue-700 sm:max-w-xs sm:text-sm sm:leading-6"
+                  >
                     <option>Seleccione una opción</option>
-                    <option>Juguetes</option>
-                    <option>Ropa</option>
+                    {categories.map((item) => (
+                      <option key={item.id} value={item.id}>
+                        {item.name}
+                      </option>
+                    ))}
                   </select>
                 </div>
               </div>
@@ -149,14 +190,19 @@ export const CreatePost = () => {
 
           {/*BUYING DETAILS*/}
           <div>
-            <h2 className="text-base font-semibold leading-7 text-gray-900">Detalles de la compra</h2>
+            <h2 className="text-base font-semibold leading-7 text-gray-900">
+              Detalles de la compra
+            </h2>
             <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-600">
               Cuéntale a tus futurois buddies las condiciones de compra.
             </p>
             <div className="mt-10 space-y-8 border-b border-gray-900/10 pb-12 sm:space-y-0 sm:divide-y sm:divide-gray-900/10 sm:border-t sm:pb-0">
               {/*MINIMUM UNITS*/}
               <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
-                <label htmlFor="min_units" className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5">
+                <label
+                  htmlFor="min_units"
+                  className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
+                >
                   Stock requerido para la compra
                 </label>
                 <div className="mt-2 sm:col-span-2 sm:mt-0">
@@ -173,7 +219,8 @@ export const CreatePost = () => {
               <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
                 <label
                   htmlFor="initial_contribution"
-                  className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5">
+                  className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
+                >
                   Contribución inicial
                 </label>
                 <div className="mt-2 sm:col-span-2 sm:mt-0">
@@ -190,7 +237,8 @@ export const CreatePost = () => {
               <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
                 <label
                   htmlFor="min_contribution"
-                  className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5">
+                  className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
+                >
                   Aporte mínimo
                 </label>
                 <div className="mt-2 sm:col-span-2 sm:mt-0">
@@ -205,7 +253,10 @@ export const CreatePost = () => {
 
               {/*UNIT PRICE*/}
               <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
-                <label htmlFor="unit_price" className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5">
+                <label
+                  htmlFor="unit_price"
+                  className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
+                >
                   Precio unitario
                 </label>
                 <div className="mt-2 sm:col-span-2 sm:mt-0">
@@ -220,7 +271,10 @@ export const CreatePost = () => {
 
               {/*EXPITRATION DATE*/}
               <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
-                <label htmlFor="exp_date" className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5">
+                <label
+                  htmlFor="exp_date"
+                  className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
+                >
                   Fecha límite del bulking
                 </label>
                 <div className="col-span-2 w-full">
@@ -232,13 +286,17 @@ export const CreatePost = () => {
         </div>
 
         <div className="mt-6 flex items-center justify-end gap-x-6">
-          <Link type="button" className="text-sm font-semibold leading-6 text-gray-900 hover:text-red-500">
+          <Link
+            type="button"
+            className="text-sm font-semibold leading-6 text-gray-900 hover:text-red-500"
+          >
             Cancel
           </Link>
 
           <button
             type="submit"
-            className="inline-flex justify-center rounded-md bg-buddies-blue-700 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-buddies-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-buddies-blue-700">
+            className="inline-flex justify-center rounded-md bg-buddies-blue-700 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-buddies-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-buddies-blue-700"
+          >
             Publicar
           </button>
         </div>
