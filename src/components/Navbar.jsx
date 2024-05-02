d3e533230b86dcc4eee90f09a954e7d7b83c1f01
Fabian Leiva <123709798+fabianleiva@users.noreply.github.com>
2024-05-01 23:46:12 -0400

    change mobile navbar links when isAuth === true

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -198,13 +198,45 @@ const Navbar = () => {
                   )}
                 </div>
                 <div className="py-6">
-                  <Link
-                    to="/auth/login"
-                    onClick={() => setMobileMenuOpen(false)}
-                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                  >
-                    Ingresar
-                  </Link>
+                {!isAuth && (
+                    <Link
+                      to="/auth/login"
+                      onClick={() => setMobileMenuOpen(false)}
+                      className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    >
+                      Ingresar
+                    </Link>
+                  )}
+                  {isAuth && (
+                    <div className="lg:flex align-middle">
+                      <Link
+                        to="/user/profile"
+                        className="flex justify-start align-middle w-full py-1 font-semibold text-buddies-blue-700  hover:text-buddies-blue-500"
+                      >
+                        <div>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            className="w-6 h-6"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                            />
+                          </svg>
+                        </div>
+                        <div className="text- font-semibold leading-6">
+                          {" "}
+                          Mi Perfil
+                        </div>
+                      </Link>
+                    </div>
+                  )}
+
                 </div>
               </div>
             </div>
