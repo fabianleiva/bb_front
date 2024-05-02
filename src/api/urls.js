b15e57d3785f5078b30a990db85a72b28f27fb2f
Fabian Leiva <fabianleivacortes@gmail.com>
2024-05-02 18:24:42 -0400

    add logout url

--- src/api/urls.js
+++ src/api/urls.js
@@ -1,9 +1,10 @@
-export const REGISTER_URL = "/register";
 export const REGISTER_GOOGLE_URL =
   "https://bulkbuddies.onrender.com/api/v1/auth/google/register";
-export const LOGIN_URL = "/login";
 export const LOGIN_GOOGLE_URL =
   "https://bulkbuddies.onrender.com/api/v1/auth/google";
-export const GET_POSTS_URL = "https://bulkbuddies.onrender.com/api/v1/post";
-export const CREATE_NEW_POST = "https://bulkbuddies.onrender.com/api/v1/post";
-export const GET_CATEGORIES_URL = "https://bulkbuddies.onrender.com/api/v1/category"
+export const REGISTER_URL = "/register";
+export const LOGIN_URL = "/login";
+export const GET_POSTS_URL = "/post";
+export const CREATE_NEW_POST = "/post";
+export const GET_CATEGORIES_URL = "/category"
+export const LOGOUT_URL = "/logout"
