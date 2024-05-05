03353a9337fb456e4a48c6be936b6421ab8a6d01
Felipe Castillo <felipecastillo48@gmail.com>
2024-05-05 17:01:13 -0400

    add import in AuthLayout

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import { Footer } from "../components/Footer";
 import { Navigate } from 'react-router-dom';
-
+import { storeBulkBuddies } from "../state/state";
 export const AuthLayout = () => {
 
   const isAuth = storeBulkBuddies((state) => state.isAuth)
